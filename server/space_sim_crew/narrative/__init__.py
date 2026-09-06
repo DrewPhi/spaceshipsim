e3479f1e9ddd40d6844453826c019d82dbddd902
Drew Steindl <Drewsteindl@icloud.com>
2026-09-06 15:59:05 -0400

    Add Universe God narrative package

---
+++ server/space_sim_crew/narrative/__init__.py
@@ -0,0 +1,5 @@
+"""Persistent, provider-neutral narrative world simulation."""
+
+from .session import UniverseGodGameSession, UniverseGodSessionManager
+
+__all__ = ["UniverseGodGameSession", "UniverseGodSessionManager"]
